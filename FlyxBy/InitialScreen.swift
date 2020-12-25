176436f89ed9c03bd11be6726a7e4903a39f521a
Mitchell Reyes <mitchellreyes@Mitchells-Air.hsd1.ca.comcast.net>
2020-12-24 20:11:06 -0800

    Initial repo for social media app on local computer

---
+++ FlyxBy/InitialScreen.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlyxBy
+//
+//  Created by Mitchell Reyes on 3/30/20.
+//  Copyright © 2020 Mitchell Reyes. All rights reserved.
+//
+
+import UIKit
+
+class InitialScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '13.0'
+
+target 'FlyxBy' do
+  use_frameworks!
+
+  # Pods for FlyxBy
+
+end
